de8d7d5242beb8c335ca765d978d2f27fc5eff62
Swamita <swamita24@gmail.com>
2020-03-25 20:06:46 +0530

    Initial Commit

---
+++ myDiceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myDiceApp
+//
+//  Created by Swamita on 25/03/20.
+//  Copyright © 2020 Swamita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
